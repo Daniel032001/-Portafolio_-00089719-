3b71cccca78896295fcb9927690f1a5c1470bef8
Daniel032001 <54299423+Daniel032001@users.noreply.github.com>
2019-09-06 12:35:54 -0600

    Add files via upload

---
+++ Ejercicio 26.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+string partereal,parteimaginaria,conjugado;
+
+int main(){
+    cout<<"Numeros Complejos:"<<endl;
+    cout<<"Digite Real"<<endl;
+    cin>>partereal;
+    cout<<"Digite parte imaginaria"<<endl;
+    cin>>parteimaginaria;
+    cout<<"El complejo ingresado es: "<<partereal<<parteimaginaria<<endl;
+   
+    }
