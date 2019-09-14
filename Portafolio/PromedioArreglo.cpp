d0d0a47726f5a64b7737436f8f88274f5bf7cba0
Daniel032001 <54299423+Daniel032001@users.noreply.github.com>
2019-09-13 22:16:26 -0600

    Add files via upload

---
+++ Portafolio/PromedioArreglo.cpp
@@ -0,0 +1,21 @@
+//JOSE DANIEL MAURICIO GUERRERO 00089719
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[10];
+    int sum=0;
+    int promedio=0;
+    
+    for(int i=0; i<10; i++);
+    cout<<"Ingrese numero: "<<endl;
+    cin>>a[1];
+    sum+=a[1];
+
+    promedio= sum/10;
+    cout<<"La suma es: "<<sum<<endl;
+    cout<<"El promedio es: "<<promedio<<endl;
+
+}
